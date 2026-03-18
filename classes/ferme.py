034f8a595f7cbb7210750ba6ddf5f88cb79d22b0
anis <anis.chmn@gmail.com>
2026-03-18 12:49:28 +0100

    repartitions fonctions dans fichiers

---
+++ classes/ferme.py
@@ -0,0 +1,18 @@
+class Ferme:
+    def __init__(self, config):
+        pass
+
+    def passer_a_la_saison_suivante(self):
+        pass
+
+    def valider_transaction(self, cout_achat):
+        pass
+
+    def appliquer_impact_ecologique(self, variation_pourcentage):
+        pass
+
+    def calculer_bilan_fin_saison(self, liste_plantes_survivantes):
+        pass
+
+    def verifier_conditions_fin_partie(self):
+        pass
